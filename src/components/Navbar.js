1026692cf7ba99457b0ff665ae53f6ed4cb0748f
surjeetvishwas <surjeetvishwas@gmail.com>
2023-08-22 18:54:21 +0530

    login funtion added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,12 @@ import "./navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 import { NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
+
   return (
     <>
       <nav className="main-nav">
@@ -15,8 +18,9 @@ const Navbar = () => {
             <span>C</span>hitrakar
             <span>S</span>tudios
           </h2>
+         
         </div>
-
+       
         <div
           className={
             showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
@@ -35,10 +39,27 @@ const Navbar = () => {
             <li>
               <NavLink to="/contact">contact</NavLink>
             </li>
+
+            {
+              isAuthenticated ? (
+                <li>
+                  <button className="btn" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button>
+                </li> 
+              ) :
+              <li>
+                <button className="btn" onClick={() => loginWithRedirect()}>Log In</button>
+              </li>
+            }
+            <li>
+          {isAuthenticated && <p>{user.name}</p>}
+        </li>
           </ul>
         </div>
 
         <div className="search">
+        
           <ul className="search-desktop">
             <i className="fa-solid fa-magnifying-glass"></i>
             <i className="fa-solid fa-cart-shopping"></i>
